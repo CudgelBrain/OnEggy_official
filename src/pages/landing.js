dec26f0053ec8ee77b693dbd411d08afe29c5b28
cudgel brain <sneh.bsspr@gmail.com>
2023-08-15 00:25:54 +0530

    changes made

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -8,12 +8,12 @@ const Home = () => {
         <img
           src="/Rectangle.png"
           alt="Image 1"
-          className="absolute top-0 right-0 z-0"
+          className="absolute top-0 right-0 z-0 opacity-20"
         />
         <img
           src="/mask.png"
           alt="Image 2"
-          className="absolute top-0 right-0 z-2"
+          className="absolute top-0 right-0 z-1 opacity-50"
         />
       </div>
       <div
@@ -37,9 +37,11 @@ const Home = () => {
               <li className="mx-5 cursor-pointer">Clients</li>
               <li className="mx-5 cursor-pointer">Team</li>
             </ul>
-            <button className="bg-l_blue hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md justify-end">
-              CONTACT
-            </button>
+            <div className="">
+              <button className="bg-l_blue hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md ml-[35rem] mt-2">
+                CONTACT
+              </button>
+            </div>
           </nav>
         </div>
         <main>
@@ -51,12 +53,12 @@ const Home = () => {
                 </span>
                 With Human Touch
               </h1>
-              <p className="pt-3">
+              <p className="pt-4">
                 Create your unique footprint in the digital world with OnEggy’s
                 IT services. Transform your business with our cutting-edge and
                 effective digital solutions to pave your path to success.
               </p>
-              <div className="buttons flex">
+              <div className="buttons flex pt-1">
                 <button className="bg-white border border-l_blue text-l_blue hover:bg-l_blue hover:text-white px-10 py-2 mt-5 rounded-md flex shadow-md">
                   <span className=" transition-transform duration-300 hover:translate-x-2">
                     WRITE TO US
@@ -85,35 +87,46 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="points grid grid-cols-3 gap- text-center">
+          <div className="points grid grid-cols-3 text-center">
             <div className=" cursor-pointer">
               <h2 className="text-4xl font-semibold hover:text-l_blue">
                 For Startups
               </h2>
-              <p className="mt-4 justify-left">
+              <p className="mt-6">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-                mollitia.
+                mollitia. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Quaerat architecto iure eos, cupiditate ut voluptates
+                maiores, recusandae ab reiciendis, hic minus eius? Architecto
+                vero laborum pariatur sit explicabo. Expedita, architecto.
               </p>
             </div>
             <div className=" cursor-pointer">
               <h2 className="text-4xl font-semibold hover:text-l_blue">
                 Free Quotation
               </h2>
-              <p className="mt-4 justify-left">
+              <p className="mt-6">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-                mollitia.
+                mollitia. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Quaerat architecto iure eos, cupiditate ut voluptates
+                maiores, recusandae ab reiciendis, hic minus eius? Architecto
+                vero laborum pariatur sit explicabo. Expedita, architecto.
               </p>
             </div>
             <div className=" cursor-pointer mb-10">
               <h2 className="text-4xl font-semibold hover:text-l_blue">
                 Free Call
               </h2>
-              <p className="mt-4 justify-left">
+              <p className="mt-6">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-                mollitia.
+                mollitia. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Quaerat architecto iure eos, cupiditate ut voluptates
+                maiores, recusandae ab reiciendis, hic minus eius? Architecto
+                vero laborum pariatur sit explicabo. Expedita, architecto.
               </p>
             </div>
           </div>
+
+          <div className="mt5"></div>
         </main>
       </div>
     </div>
