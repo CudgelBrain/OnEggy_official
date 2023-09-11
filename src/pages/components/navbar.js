326cca6d79f15bbb9155a30eed2b84343b7a02ff
cudgel brain <sneh.bsspr@gmail.com>
2023-09-11 07:15:21 +0530

    completed services page

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -14,16 +14,27 @@ const navbar = () => {
           className="logo_img cursor-pointer"
         />
       </a>
-      <ul className="flex justify-end py-4 align-center">
+      <ul className="flex justify-end py-4">
         <a href="../about/about">
           <li className="mx-5 cursor-pointer pt-2">About Us</li>
         </a>
-        <li className="mx-5 cursor-pointer pt-2">Service</li>
-        <a href="../team/team"><li className="mx-5 cursor-pointer pt-2">Team</li></a>
-        <a href="../client/client"><li className="mx-5 cursor-pointer pt-2">Portfolio</li></a>
-        <a href="../blog/blog"><li className="mx-5 cursor-pointer pt-2">Blog</li></a>
-        <Link href="/contact/section1" className="text-black py-2 px-6 rounded-md border border-black">
-            Request a Quote
+        <a href="../services/services">
+          <li className="mx-5 cursor-pointer pt-2">Service</li>
+        </a>
+        <a href="../team/team">
+          <li className="mx-5 cursor-pointer pt-2">Team</li>
+        </a>
+        <a href="../client/client">
+          <li className="mx-5 cursor-pointer pt-2">Portfolio</li>
+        </a>
+        <a href="../blog/blog">
+          <li className="mx-5 cursor-pointer pt-2">Blog</li>
+        </a>
+        <Link
+          href="/contact/contact"
+          className="text-black py-2 px-6 rounded-md border border-black"
+        >
+          Request a Quote
         </Link>{" "}
       </ul>
     </nav>
